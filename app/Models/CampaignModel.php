3f7eff158f97e60e49f8b29cef7778e41cae6c31
Muhammad Haris <61509864+muhharis99@users.noreply.github.com>
2026-09-13 01:29:01 +0700

    feat: add campaign model

---
+++ app/Models/CampaignModel.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+class CampaignModel extends Model
+{
+    protected $table='campaigns'; protected $primaryKey='id'; protected $returnType='array';
+    protected $allowedFields=['tenant_id','name','template_id','message_body','segment_json','scheduled_at','timezone','status','rate_limit_per_minute','total_recipients','sent_count','delivered_count','read_count','failed_count','created_by']; protected $useTimestamps=true;
+}
