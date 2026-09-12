50596d7007aae7136a9660c7571e6d1b8b49b0b8
Muhammad Haris <61509864+muhharis99@users.noreply.github.com>
2026-09-13 05:52:59 +0700

    feat(queue): allow campaign WhatsApp device selection

--- app/Models/CampaignModel.php
+++ app/Models/CampaignModel.php
@@ -4,5 +4,5 @@ use CodeIgniter\Model;
 class CampaignModel extends Model
 {
     protected $table='campaigns'; protected $primaryKey='id'; protected $returnType='array';
-    protected $allowedFields=['tenant_id','name','template_id','message_body','segment_json','scheduled_at','timezone','status','rate_limit_per_minute','total_recipients','sent_count','delivered_count','read_count','failed_count','created_by']; protected $useTimestamps=true;
+    protected $allowedFields=['tenant_id','device_id','name','template_id','message_body','segment_json','scheduled_at','timezone','status','rate_limit_per_minute','total_recipients','sent_count','delivered_count','read_count','failed_count','created_by']; protected $useTimestamps=true;
 }
