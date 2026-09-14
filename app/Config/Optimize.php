67ee3f58eb025cff4aa7e6fd26717435d7c74231
Muhammad Haris <61509864+muhharis99@users.noreply.github.com>
2026-09-14 17:24:40 +0700

    fix: restore CI4 optimization configuration

---
+++ app/Config/Optimize.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Config;
+
+/**
+ * Optimization configuration.
+ */
+class Optimize
+{
+    public bool $configCacheEnabled = false;
+    public bool $locatorCacheEnabled = false;
+}
