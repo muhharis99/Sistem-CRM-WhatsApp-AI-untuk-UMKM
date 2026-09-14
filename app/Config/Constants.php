a36bd99b34cd0b93d12a300cbb3f58e2fb19a077
Muhammad Haris <61509864+muhharis99@users.noreply.github.com>
2026-09-14 17:12:39 +0700

    fix: add missing CodeIgniter Constants bootstrap file

---
+++ app/Config/Constants.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Constants
+ *
+ * Define application constants used during CodeIgniter bootstrap.
+ */
+
+// Error Reporting Level
+// Depending on the environment, CodeIgniter's Boot class will configure
+// the appropriate reporting level. Keep application constants minimal here.
