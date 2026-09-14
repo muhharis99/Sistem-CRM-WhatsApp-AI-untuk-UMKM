4ac5ce3f26907884a9e8581432350a66a0b12dd8
Muhammad Haris <61509864+muhharis99@users.noreply.github.com>
2026-09-14 17:00:33 +0700

    Fix Modules config bootstrap compatibility

--- app/Config/Modules.php
+++ app/Config/Modules.php
@@ -13,8 +13,11 @@ class Modules extends BaseConfig
 
     /**
      * Paths where CodeIgniter should discover modules.
+     *
+     * Keep this empty because the current application does not use
+     * Composer-discovered CodeIgniter modules. Using APPPATH here would
+     * require the APPPATH constant before Config\Modules is instantiated
+     * during Spark bootstrap.
      */
-    public array $discoverIn = [
-        APPPATH,
-    ];
+    public array $discoverIn = [];
 }
