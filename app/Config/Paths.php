c05f586bcf32cbb08c44b6758f0d0a264b3d8745
Muhammad Haris <61509864+muhharis99@users.noreply.github.com>
2026-09-14 17:11:10 +0700

    fix: align Paths config with CI4 4.7

--- app/Config/Paths.php
+++ app/Config/Paths.php
@@ -2,13 +2,21 @@
 
 namespace Config;
 
-use CodeIgniter\Config\BaseConfig;
-
-class Paths extends BaseConfig
+/**
+ * Paths
+ *
+ * Holds the paths that are used by the system to locate the main
+ * directories, app, system, etc.
+ *
+ * NOTE: This class is required prior to Autoloader instantiation,
+ * and does not extend BaseConfig.
+ */
+class Paths
 {
     public string $systemDirectory = __DIR__ . '/../../vendor/codeigniter4/framework/system';
     public string $appDirectory = __DIR__ . '/..';
     public string $writableDirectory = __DIR__ . '/../../writable';
     public string $testsDirectory = __DIR__ . '/../../tests';
     public string $viewDirectory = __DIR__ . '/../Views';
+    public string $envDirectory = __DIR__ . '/../../';
 }
