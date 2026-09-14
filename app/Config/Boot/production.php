874cd842366ad8dbfb2af41d1fe3b0f5138767b8
Muhammad Haris <61509864+muhharis99@users.noreply.github.com>
2026-09-14 17:24:31 +0700

    fix: restore CI4 production bootstrap

---
+++ app/Config/Boot/production.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ |--------------------------------------------------------------------------
+ | ERROR DISPLAY
+ |--------------------------------------------------------------------------
+ */
+error_reporting(E_ALL & ~E_DEPRECATED);
+ini_set('display_errors', '0');
+
+defined('CI_DEBUG') || define('CI_DEBUG', false);
